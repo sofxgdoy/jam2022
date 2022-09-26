2d50bd6293ebefa30ec52b09def4c12f15d6b8a7
Alan Medina <alanmedinaalan@hotmail.com>
2022-09-26 00:18:29 -0300

    Why
    Why're still here, just to suffer

--- Assets/Scripts/PlayerMov.cs
+++ Assets/Scripts/PlayerMov.cs
@@ -15,14 +15,19 @@ public class PlayerMov : MonoBehaviour
     float horizontalNegative = 0f;
     bool salto = false;
 
+    private Animator anim;
+    private Rigidbody2D rb;
+
     public static bool muerteTiempo = false;
 
     public bool drvita = false;
     public bool cambio = false;
-
+    
     //private shake shake;
     void Start()
     {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
         //shake = GameObject.FindGameObjectsWithTag("ScreenShake").GetComponent<Shake>();
     }
 
@@ -32,32 +37,57 @@ public class PlayerMov : MonoBehaviour
         horizontalMove = Input.GetAxisRaw("Horizontal")* runSpeed;
         horizontalNegative = Input.GetAxisRaw("Horizontal")*-1 * runSpeed;
 
-        if (Input.GetButtonDown("Jump")) {
-
-            salto = true;
-
-        }
 
         if (muerteTiempo) {
             Muerte();
             muerteTiempo = false;
         }
 
-        /*if (cambio == false) {
 
-        }*/
     }
 
     void FixedUpdate(){
         if (drvita == true) {
+            if (Input.GetButtonDown("Jump")) {
+            salto = true;
+            anim.SetBool("Jump", true);
+        }
+            else{
+                anim.SetBool("Jump", false);
+            }
+
             controller.Move(horizontalMove * Time.fixedDeltaTime, false, salto);
             salto = false;
-
+            
+            if (horizontalMove == 0){
+                anim.SetBool("Move", false);
+            }
+            else{
+                anim.SetBool("Move", true);
+            }
+          
         }
+        //Caede
         else {
+            if (Input.GetButtonDown("Jump")) {
+            salto = true;
+            anim.SetBool("Jump", true);
+        }
+            else{
+                anim.SetBool("Jump", false);
+            }
             controller.Move(horizontalNegative * Time.fixedDeltaTime, false, salto);
             salto = false;
+
+            if (horizontalMove == 0){
+                anim.SetBool("Move", false);
+            }
+            else{
+                anim.SetBool("Move", true);
+            }
+
         }
+
         
     }
     
@@ -66,7 +96,7 @@ public class PlayerMov : MonoBehaviour
     }
 
     void Transformacion() {
-        
+        //set.trigger("pedos", true);
         drvita = !drvita;
         //animator.Play("CameraShake");
     }
